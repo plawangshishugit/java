f4db447cab02a44b9aa728691e3ab368a829e010
Plawang Shishu <plawangshishu5029@gmail.com>
2024-04-30 23:43:45 +0530

    springdemo

---
+++ src/test/java/com/plawangshishu/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plawangshishu.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
